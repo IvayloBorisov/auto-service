f434cce8e9eae1bdc9875a21c6f3228021bd92b3
IvayloBorisov <ivaylo_borisov@abv.bg>
2021-03-02 12:39:02 +0200

    Create Header structurte (Logo, Navbar and ListItem components)

---
+++ components/Logo/Logo.js
@@ -0,0 +1,11 @@
+
+const Logo = () => {
+
+    return (
+        <div>
+            <h1><span>CUSTOM</span>GARAGE</h1>
+        </div>
+    )
+}
+
+export default Logo;
---
+++ helper/navigation.js
@@ -0,0 +1,23 @@
+export const navLinks = [
+    {
+        id: 1,
+        title: "Начало"
+    },
+    {
+        id: 2,
+        title: "За нас"
+    },
+    {
+        id: 3,
+        title: "Услуги"
+    },
+    {
+        id: 4,
+        title: "Галерия",
+        href : "/gallery"
+    },
+    {
+        id: 5,
+        title: "Контакти"
+    }
+];
